bbe4547d886aabc2e3523e8c6699ed7d54f3e190
William Mittard <billymittard@ymail.com>
2020-04-29 22:50:23 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -14,4 +14,29 @@ class Appointment
    end 
  end 
   
-  
+  class Appointment
+
+    attr_reader :patient, :doctor, :date
+
+    @@all = []
+
+    def initialize(patient ="", doctor = "", date = "")
+     @patient = patient
+     @doctor = doctor
+     @date = date
+     @@all << self
+   end
+
+    def self.all
+     @@all
+   end
+
+    def patients
+     self.patient
+   end
+
+    def doctors
+     self.doctor
+   end
+
+  end
